65061aefd0d14296f3990dbeefde884f946f02b1
Kumar Tallapalli <kumar.tallapalli@criticalriver.com>
2022-10-31 15:18:18 +0530

    redirect script change

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -2,10 +2,6 @@
 $(document).ready(function(){
 	try{
 		
-		let locationHrefPath = window.location.href;
-		if(locationHrefPath.endsWith("/latest"))
-			window.location.href = locationHrefPath+"/";
-		
 	$('a[href*="cypresssemiconductorco"]').each(function(){ 
 		var oldUrl = $(this).attr("href"); 
 		var sampleText = $(this).text().replace("cypresssemiconductorco", "Infineon");
